b141cacb9b6a1540728906bd9268c1ae1aa0cde0
Mychal Thompson <mychalrthompson@gmail.com>
2018-09-09 12:34:26 -0400

    Updated @Reactive() deprecation notice

--- src/component.ts
+++ src/component.ts
@@ -1,5 +1,5 @@
 /** @ClassDecoratorFactory */
 export function Reactive(): ClassDecorator {
-    console.info("DEPRECATION NOTICE: The @Reactive() class decorator is no longer used and will be removed in a future version of @lithiumjs/angular.");
+    console.info("DEPRECATION NOTICE: The @Reactive() class decorator is no longer used and can be removed from your code. It will be removed in a future version of @lithiumjs/angular.");
     return function () {};
 }

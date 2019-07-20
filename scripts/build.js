5ca34fc48052bca15417c61ee439d68bb0998829
Mychal Thompson <mychalrthompson@gmail.com>
2019-07-20 00:12:40 -0400

    Added APF support to build

--- scripts/build.js
+++ scripts/build.js
@@ -17,9 +17,25 @@ const BUILD_DIR = "./dist";
 (function main() {
     fs.removeSync(BUILD_DIR);
 
-    child_process.execSync("ngc");
+    // UMD
+    compile("es5", undefined, `--rootDir ./src --module amd --outFile ${BUILD_DIR}/bundles/lithium.umd.js`);
+
+    // ES5, ES2015
+    compile("es5", "esm5", "--rootDir ./src --declaration");
+    compile("es2015", "esm2015", "--rootDir ./src --declaration");
+
+    // Typings
+    compile("esnext", undefined, " --declaration --emitDeclarationOnly", "tsc");
 
     for (let injectedFileName in INJECTED_FILES) {
         fs.copy(INJECTED_FILES[injectedFileName], path.join(BUILD_DIR, injectedFileName));
     }
 })();
+
+function compile(target, outDir, options, compiler) {
+    outDir = outDir || "";
+    options = options || "";
+    compiler = compiler || "ngc";
+
+    child_process.execSync(`${compiler} -p tsconfig.json --target ${target} --outDir ./dist/${outDir} ${options}`);
+}

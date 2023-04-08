c38681ffc23cce913ecaeb3cb8df44b605b1168d
Mychal Thompson <mychalrthompson@gmail.com>
2023-04-07 21:08:31 -0400

    7.2.2: Update to add support for Angular 15.

--- spec/helpers/main.ts
+++ spec/helpers/main.ts
@@ -1,4 +1,11 @@
 import "source-map-support/register";
+
+/* TODO 2023-04-07 - IMPORTANT TESTING NOTE!
+ * Angular `TestBed` requires zone to be imported even though no tests require it.
+ * However, zone.js is currently incompatible with Promises in Node v16.17.1 or greater.
+ * Because of this, unit tests can currently only be run in Node versions before v16.17.1.
+ * See https://github.com/angular/angular/issues/48198
+*/
 import "zone.js";
 import "zone.js/testing";
 
